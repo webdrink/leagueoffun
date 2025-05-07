309464a6633e36e966c1fabee3f9b050a4029d4c
Patrick Bziuk <patrick@bziuk.com>
2025-05-07 22:39:30 +0200

    Add ESLint configuration file and update dependencies for React and TypeScript support

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+// filepath: .eslintrc.js
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    // Add custom rules here
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 
+      argsIgnorePattern: '^_',
+      varsIgnorePattern: '^_' 
+    }],
+    // Relaxed rule for development
+    '@typescript-eslint/no-explicit-any': 'warn'
+  }
+};
